cdf0c36c4e584ba350b9e9f45f2a42a1c1c4671e
herehr <alles@voiguad.at>
2025-07-23 15:25:20 +0200

    index.ts updated with animalRoutes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth';
+import animalRoutes from './routes/animal';
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes); // ✅ Important
+app.use('/api/animals', animalRoutes);
 
 app.get('/', (req, res) => {
   res.send('Dogpoint backend running');
